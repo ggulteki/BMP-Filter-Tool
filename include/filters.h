63377eb48ad51525a05cc1fb69d46cc505fba066
ggulteki <gokberkgultekin96@gmail.com>
2024-12-16 19:40:39 +0300

    Added header guard

--- include/filters.h
+++ include/filters.h
@@ -1,15 +1,19 @@
+#ifndef FILTERS_H
+#define FILTERS_H
+
 #include <stdint.h>
-
-#include <getopt.h>
 #include <math.h>
-
 #include <stdio.h>
 #include <stdlib.h>
+
+#define BITMAP_HEADER_SIZE 54
+#define BITMAP_TYPE 0x4d42
+#define BITMAP_COMPRESSION 0
 
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
-typedef uint16_t WORD;
+typedef uint16_t WORD; 
 
 typedef struct
 {
@@ -18,8 +22,7 @@ typedef struct
     WORD   bfReserved1;
     WORD   bfReserved2;
     DWORD  bfOffBits;
-} __attribute__((__packed__))
-BITMAPFILEHEADER;
+} __attribute__((__packed__)) BITMAPFILEHEADER;
 
 typedef struct
 {
@@ -34,18 +37,18 @@ typedef struct
     LONG   biYPelsPerMeter;
     DWORD  biClrUsed;
     DWORD  biClrImportant;
-} __attribute__((__packed__))
-BITMAPINFOHEADER;
+} __attribute__((__packed__)) BITMAPINFOHEADER;
 
 typedef struct
 {
     BYTE  rgbtBlue;
     BYTE  rgbtGreen;
     BYTE  rgbtRed;
-} __attribute__((__packed__))
-RGBTRIPLE;
+} __attribute__((__packed__)) RGBTRIPLE;
 
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 void edges(int height, int width, RGBTRIPLE image[height][width]);
 void blur(int height, int width, RGBTRIPLE image[height][width]);
+
+#endif
