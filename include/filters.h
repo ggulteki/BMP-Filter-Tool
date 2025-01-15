058f5f9a539abc5fe3cfc5fd0b9a33fe5598f7e7
ggulteki <gokberkgultekin96@gmail.com>
2025-01-15 15:38:03 +0300

    Add comments for clarity

--- include/filters.h
+++ include/filters.h
@@ -1,3 +1,8 @@
+/*
+ * filters.h
+ * Image filtering functions and bitmap structure definitions
+ */
+
 #ifndef FILTERS_H
 #define FILTERS_H
 
@@ -6,49 +11,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BITMAP_HEADER_SIZE 54
-#define BITMAP_TYPE 0x4d42
-#define BITMAP_COMPRESSION 0
+/* Bitmap file constants */
+#define BITMAP_HEADER_SIZE    54
+#define BITMAP_TYPE          0x4d42
+#define BITMAP_COMPRESSION   0
 
+/* Type definitions for bitmap structures */
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
-typedef uint16_t WORD; 
+typedef uint16_t WORD;
 
-typedef struct
-{
-    WORD   bfType;
-    DWORD  bfSize;
-    WORD   bfReserved1;
-    WORD   bfReserved2;
-    DWORD  bfOffBits;
+/*
+ * Bitmap file header structure
+ * Contains information about the type, size, and layout of a bitmap file
+ */
+typedef struct {
+    WORD   bfType;           /* Bitmap type identifier */
+    DWORD  bfSize;           /* Size of the bitmap file */
+    WORD   bfReserved1;      /* Reserved field */
+    WORD   bfReserved2;      /* Reserved field */
+    DWORD  bfOffBits;        /* Offset to bitmap data */
 } __attribute__((__packed__)) BITMAPFILEHEADER;
 
-typedef struct
-{
-    DWORD  biSize;
-    LONG   biWidth;
-    LONG   biHeight;
-    WORD   biPlanes;
-    WORD   biBitCount;
-    DWORD  biCompression;
-    DWORD  biSizeImage;
-    LONG   biXPelsPerMeter;
-    LONG   biYPelsPerMeter;
-    DWORD  biClrUsed;
-    DWORD  biClrImportant;
+/*
+ * Bitmap information header structure
+ * Contains information about the dimensions and format of the bitmap image
+ */
+typedef struct {
+    DWORD  biSize;           /* Size of this header */
+    LONG   biWidth;          /* Image width in pixels */
+    LONG   biHeight;         /* Image height in pixels */
+    WORD   biPlanes;         /* Number of color planes */
+    WORD   biBitCount;       /* Bits per pixel */
+    DWORD  biCompression;    /* Compression type */
+    DWORD  biSizeImage;      /* Size of image data */
+    LONG   biXPelsPerMeter;  /* Horizontal resolution */
+    LONG   biYPelsPerMeter;  /* Vertical resolution */
+    DWORD  biClrUsed;        /* Number of colors used */
+    DWORD  biClrImportant;   /* Number of important colors */
 } __attribute__((__packed__)) BITMAPINFOHEADER;
 
-typedef struct
-{
-    BYTE  rgbtBlue;
-    BYTE  rgbtGreen;
-    BYTE  rgbtRed;
+/*
+ * RGB triple structure
+ * Represents a pixel's color values in BGR order
+ */
+typedef struct {
+    BYTE  rgbtBlue;         /* Blue component */
+    BYTE  rgbtGreen;        /* Green component */
+    BYTE  rgbtRed;          /* Red component */
 } __attribute__((__packed__)) RGBTRIPLE;
 
+/* Function prototypes */
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 void edges(int height, int width, RGBTRIPLE image[height][width]);
 void blur(int height, int width, RGBTRIPLE image[height][width]);
 
-#endif
+#endif /* FILTERS_H */
